9e80718f03f31681d605a0337f509d60b7213679
shwars <dmitri@soshnikov.com>
2014-04-08 19:28:44 +0400

    Version 1 Step 2 - Adding Photo Taking and simple picture store

--- PhotoDiary/MainPage.xaml.cs
+++ PhotoDiary/MainPage.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using PhotoDiary.Resources;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace PhotoDiary
 {
@@ -20,6 +22,23 @@ namespace PhotoDiary
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var camera = new CameraCaptureTask();
+            camera.Completed += camera_Completed;
+            camera.Show();
+        }
+
+        void camera_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                var bmp = new BitmapImage();
+                bmp.SetSource(e.ChosenPhoto);
+                PictureStore.AddPicture(new ImageInfo() { Image = bmp, Taken = DateTime.Now });
+            }
         }
 
         // Sample code for building a localized ApplicationBar
---
+++ PhotoDiary/PictureStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PhotoDiary
+{
+
+    public struct ImageInfo
+    {
+        public BitmapImage Image { get; set; }
+        public DateTime Taken { get; set; }
+    }
+
+    public class PictureStore
+    {
+        public static List<ImageInfo> piclist = new List<ImageInfo>();
+        public static void AddPicture(ImageInfo Inf)
+        { 
+            piclist.Add(Inf);
+        }
+
+        public List<ImageInfo> Images { get { return piclist; } }
+
+    }
+}
